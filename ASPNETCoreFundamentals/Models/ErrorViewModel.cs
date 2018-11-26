6ce1786bb32701825ea8d31ffcc21b6cf882235a
qishichang <qishichang@live.com>
2018-11-26 17:57:52 +1300

    Add project files.

---
+++ ASPNETCoreFundamentals/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASPNETCoreFundamentals.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
