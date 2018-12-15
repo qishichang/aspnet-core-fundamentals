0c17c412880b62246fd5653643a54b569d4cdcbe
qishichang <qishichang@live.com>
2018-12-15 21:50:42 +1300

    add FakeTextBoxFor extension method

--- ASPNETCoreFundamentals/Extensions/MyHtmlHelperExtensions.cs
+++ ASPNETCoreFundamentals/Extensions/MyHtmlHelperExtensions.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace Microsoft.AspNetCore.Mvc.Rendering
@@ -16,6 +17,20 @@ namespace Microsoft.AspNetCore.Mvc.Rendering
             var tagBuilder = new TagBuilder(tagName);
             tagBuilder.Attributes.Add("style", $"color: {color ?? "green"}");
             tagBuilder.InnerHtml.Append(content ?? string.Empty);
+            return tagBuilder;
+        }
+
+        public static IHtmlContent FakeTextboxFor<TModel, TResult>(this IHtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TResult>> expression)
+        {
+            var body = expression.Body as MemberExpression;
+            var propertyName = body.Member.Name.ToLower(); // keep lower case
+            var tagBuilder = new TagBuilder("input")
+            {
+                TagRenderMode = TagRenderMode.SelfClosing
+            };
+            tagBuilder.Attributes.Add("type", "text"); // can be omitted
+            tagBuilder.Attributes.Add("name", propertyName); // key purpose 1: for name-value pair
+            tagBuilder.Attributes.Add("id", propertyName); // key purpose 2: for client JavaScript and CSS
             return tagBuilder;
         }
     }
