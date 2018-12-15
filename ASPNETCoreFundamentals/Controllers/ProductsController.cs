08a4bfadf6f4d96c66caaaa3c29de307f2840e6d
qishichang <qishichang@live.com>
2018-12-15 23:12:54 +1300

    ActionLink demo

--- ASPNETCoreFundamentals/Controllers/ProductsController.cs
+++ ASPNETCoreFundamentals/Controllers/ProductsController.cs
@@ -41,5 +41,20 @@ namespace ASPNETCoreFundamentals.Controllers
             };
             return View("ProductList", vm);
         }
+
+        public IActionResult Edit()
+        {
+            var product = new Product(901, "Rocket", 99.99);
+            return View(product);
+        }
+
+        public IActionResult Show()
+        {
+            var products = new List<Product>();
+            products.Add(new Product(101, "Apple", 19.99));
+            products.Add(new Product(201, "Bike", 29.99));
+            products.Add(new Product(301, "Couch", 39.99));
+            return View(products);
+        }
     }
 }
