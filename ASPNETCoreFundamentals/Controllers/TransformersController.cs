bb3d94cb7ac10fb920e3cb319db860398a33701e
qishichang <qishichang@live.com>
2018-12-18 12:10:50 +1300

    add transformers list

---
+++ ASPNETCoreFundamentals/Controllers/TransformersController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASPNETCoreFundamentals.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNETCoreFundamentals.Controllers
+{
+    public class TransformersController : Controller
+    {
+        public IActionResult Index()
+        {
+            var roles = new List<Transformer>();
+            roles.Add(new Transformer(1, "Optimus Prime", "Earth", "Blue"));
+            roles.Add(new Transformer(2, "Bumblebee", "Earth", "Yellow"));
+            roles.Add(new Transformer(3, "Starscream", "Cybertron", "Red"));
+            roles.Add(new Transformer(4, "Soundwave", "Cybertron", "Purple"));
+            return View(roles);
+        }
+    }
+}
---
+++ ASPNETCoreFundamentals/Models/Transformer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCoreFundamentals.Models
+{
+    public class Transformer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public string Color { get; set; }
+
+        public Transformer(int id, string name, string location, string color)
+        {
+            this.ID = id;
+            this.Name = name;
+            this.Location = location;
+            this.Color = color;
+        }
+    }
+}
