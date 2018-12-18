b268aff3a508d51d853d2dbdfcc2da5141beb530
qishichang <qishichang@live.com>
2018-12-18 17:49:03 +1300

    update file

---
+++ ASPNETCoreFundamentals/Models/Country.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCoreFundamentals.Models
+{
+    public class Country
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Continent { get; set; }
+        public string NationalFlagPath { get; set; } = "Images/Default.png";
+
+        public Country(string code, string name, string continent)
+        {
+            this.Code = code;
+            this.Name = name;
+            this.Continent = continent;
+        }
+
+    }
+}
