f18c9d3f51c7b6efdff8dc93ea5dbbad9a2061e4
qishichang <qishichang@live.com>
2019-01-26 23:12:50 +1300

    Call services from main

--- ASPNETCoreFundamentals/Program.cs
+++ ASPNETCoreFundamentals/Program.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using ASPNETCoreFundamentals.Core;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace ASPNETCoreFundamentals
@@ -14,7 +16,24 @@ namespace ASPNETCoreFundamentals
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var serviceScope = host.Services.CreateScope())
+            {
+                var services = serviceScope.ServiceProvider;
+                try
+                {
+                    var serviceContext = services.GetRequiredService<IMyDependency>();
+                    serviceContext.WriteMessage("Program.Main created this message.").Wait();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
