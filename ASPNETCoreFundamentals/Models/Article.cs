610bb93ef5452d382bcc3026f4c5475df2963f02
qishichang <qishichang@live.com>
2019-01-30 20:16:23 +1300

    Partial view

---
+++ ASPNETCoreFundamentals/Models/Article.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCoreFundamentals.Models
+{
+    public class Article
+    {
+        public string AuthorName { get; set; }
+        public DateTime PublicationDate { get; set; } = DateTime.Today;
+        public string Title { get; set; }
+        public List<ArticleSection> Sections { get; set; } = new List<ArticleSection>();
+
+    }
+}
---
+++ ASPNETCoreFundamentals/Models/ArticleSection.cs
@@ -0,0 +1,8 @@
+﻿namespace ASPNETCoreFundamentals.Models
+{
+    public class ArticleSection
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
