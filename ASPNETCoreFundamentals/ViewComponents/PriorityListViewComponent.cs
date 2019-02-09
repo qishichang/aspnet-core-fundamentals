e046adb67e4dc66bc42a54fff81b516fb7685cd0
qishichang <qishichang@live.com>
2019-02-09 22:59:02 +1300

    Add PriorityListViewComponent

---
+++ ASPNETCoreFundamentals/ViewComponents/PriorityListViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using ASPNETCoreFundamentals.Data;
+using ASPNETCoreFundamentals.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCoreFundamentals.ViewComponents
+{
+    public class PriorityListViewComponent : ViewComponent
+    {
+        private readonly TodoContext _context;
+
+        public PriorityListViewComponent(TodoContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int maxPriority, bool isDone)
+        {
+            var items = await GetItemsAsync(maxPriority, isDone);
+            return View(items);
+        }
+
+        private Task<List<ToDoItem>> GetItemsAsync(int maxPriority, bool isDone) =>
+            _context.Todo.Where(x => x.IsDone == isDone && x.Priority <= maxPriority).ToListAsync();
+    }
+}
