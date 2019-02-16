058c40146398b11ed0656673b0fde59f060b7553
qishichang <qishichang@live.com>
2019-02-16 21:49:35 +1300

    Named component view

--- ASPNETCoreFundamentals/ViewComponents/PriorityListViewComponent.cs
+++ ASPNETCoreFundamentals/ViewComponents/PriorityListViewComponent.cs
@@ -20,8 +20,14 @@ namespace ASPNETCoreFundamentals.ViewComponents
 
         public async Task<IViewComponentResult> InvokeAsync(int maxPriority, bool isDone)
         {
+            string MyView = "Default";
+            // If asking for all completed tasks, render with the "PVC" view.
+            if (maxPriority > 3 && isDone == true)
+            {
+                MyView = "PVC";
+            }
             var items = await GetItemsAsync(maxPriority, isDone);
-            return View(items);
+            return View(MyView,items);
         }
 
         private Task<List<ToDoItem>> GetItemsAsync(int maxPriority, bool isDone) =>
