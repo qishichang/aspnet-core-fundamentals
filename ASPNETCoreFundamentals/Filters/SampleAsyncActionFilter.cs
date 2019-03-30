cb86f19cdd76fddd61163be144b8135c9759e042
qishichang <qishichang@live.com>
2019-03-31 01:53:05 +1300

    Filter implementation

---
+++ ASPNETCoreFundamentals/Filters/SampleAsyncActionFilter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCoreFundamentals.Filters
+{
+    public class SampleAsyncActionFilter : IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            // do something before the action executes
+            var resultContext = await next();
+            // do something after the action executes, resultContext.Result will be set
+        }
+    }
+}
