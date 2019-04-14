7bb08c974e83d2f89e86f660ae1d568480668cff
qishichang <qishichang@live.com>
2019-04-14 20:27:08 +0800

    A simple Web API controller

---
+++ ASPNETCoreFundamentals/API/FruitController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCoreFundamentals.API
+{
+    public class FruitController : Controller
+    {
+        List<string> _fruits = new List<string>
+        {
+            "Pear",
+            "Lemon",
+            "Peach"
+        };
+
+        public IEnumerable<string> Index()
+        {
+            return _fruits;
+        }
+    }
+}
