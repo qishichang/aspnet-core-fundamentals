8bb95d9085b4eba1d5fc3a623b35b49de61698db
qishichang <qishichang@live.com>
2019-04-18 20:44:33 +0800

    Attribute routing

---
+++ ASPNETCoreFundamentals/API/CarController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNETCoreFundamentals.API
+{
+    public class CarController : Controller
+    {
+        [Route("car/start")]
+        [Route("car/ignition")]
+        [Route("start-car")]
+        public IActionResult Start()
+        {
+            return Ok("The car is starting.");
+        }
+
+        [Route("car/speed/{speed}")]
+        [Route("set-speed/{speed}")]
+        public IActionResult SetCarSpeed(int speed)
+        {
+            return Ok($"The speed of the car is {speed}.");
+        }
+    }
+}
