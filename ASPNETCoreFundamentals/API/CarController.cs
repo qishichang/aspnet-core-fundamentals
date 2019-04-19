dc853fcd33bad8496b34e4d23db7a54ad1b446bb
qishichang <qishichang@live.com>
2019-04-19 15:39:00 +0800

    Add Xml formatter

--- ASPNETCoreFundamentals/API/CarController.cs
+++ ASPNETCoreFundamentals/API/CarController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ASPNETCoreFundamentals.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -23,6 +24,12 @@ namespace ASPNETCoreFundamentals.API
         public IActionResult SetCarSpeed(int speed)
         {
             return Ok($"The speed of the car is {speed}.");
+        }
+
+        [HttpPost]
+        public IActionResult Add([FromBody]Car car)
+        {
+            return Ok(car);
         }
     }
 }
---
+++ ASPNETCoreFundamentals/Models/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCoreFundamentals.Models
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+    }
+}
