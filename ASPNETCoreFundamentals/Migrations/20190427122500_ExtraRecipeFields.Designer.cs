19023c6c09549e13a07b8359c3d608bb59859b7d
qishichang <qishichang@live.com>
2019-04-27 20:29:19 +0800

    EF Core - Update database with migrations

--- ASPNETCoreFundamentals/Migrations/20190427122500_ExtraRecipeFields.Designer.cs
+++ ASPNETCoreFundamentals/Migrations/20190427122500_ExtraRecipeFields.Designer.cs
@@ -4,14 +4,16 @@ using ASPNETCoreFundamentals.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ASPNETCoreFundamentals.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190427122500_ExtraRecipeFields")]
+    partial class ExtraRecipeFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- ASPNETCoreFundamentals/Models/Recipe.cs
+++ ASPNETCoreFundamentals/Models/Recipe.cs
@@ -12,6 +12,8 @@ namespace ASPNETCoreFundamentals.Models
         public TimeSpan TimeToCook { get; set; }
         public bool IsDeleted { get; set; }
         public string Method { get; set; }
+        public bool IsVegetarian { get; set; }
+        public bool IsVegan { get; set; }
 
         public ICollection<Ingredient> Ingredients { get; set; }
     }
