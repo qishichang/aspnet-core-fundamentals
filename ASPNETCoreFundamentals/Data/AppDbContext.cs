be16121f77e155737eac1dc6c7f6fed5f9d7612b
qishichang <qishichang@live.com>
2019-04-27 20:01:25 +0800

    EF Core - Data models for recipe

---
+++ ASPNETCoreFundamentals/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using ASPNETCoreFundamentals.Models;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCoreFundamentals.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Recipe> Recipes { get; set; }
+    }
+}
---
+++ ASPNETCoreFundamentals/Models/Ingredient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCoreFundamentals.Models
+{
+    public class Ingredient
+    {
+        public int IngredientId { get; set; }
+        public int RecipeId { get; set; }
+        public string Name { get; set; }
+        public decimal Quantity { get; set; }
+        public string Unit { get; set; }
+    }
+}
