9eb51b1d71191a04a726477e581481be91b1b79b
qishichang <qishichang@live.com>
2019-05-09 11:30:21 +0800

    A simple resource fitler

---
+++ ASPNETCoreFundamentals/Filters/LogResourceFilter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCoreFundamentals.Filters
+{
+    public class LogResourceFilter : Attribute, IResourceFilter
+    {
+        public void OnResourceExecuting(ResourceExecutingContext context)
+        {
+            Console.WriteLine("Executing!");
+        }
+
+        public void OnResourceExecuted(ResourceExecutedContext context)
+        {
+            Console.WriteLine("Executed");
+        }
+    }
+}
