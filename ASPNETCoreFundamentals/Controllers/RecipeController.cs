e4749f98dbd03c3a6e9eaf90397aebe2e04536c7
qishichang <qishichang@live.com>
2019-05-13 15:22:57 +0800

    Inject ILogger into a class and write a log message

--- ASPNETCoreFundamentals/Controllers/RecipeController.cs
+++ ASPNETCoreFundamentals/Controllers/RecipeController.cs
@@ -4,20 +4,26 @@ using System.Linq;
 using System.Threading.Tasks;
 using ASPNETCoreFundamentals.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace ASPNETCoreFundamentals.Controllers
 {
     public class RecipeController : Controller
     {
         private readonly RecipeService _service;
+        private readonly ILogger<RecipeService> _log;
 
-        public RecipeController(RecipeService service)
+        public RecipeController(RecipeService service,
+                ILogger<RecipeService> log)
         {
             _service = service;
+            _log = log;
         }
         public IActionResult Index()
         {
-            return View();
+            var models = _service.GetRecipes();
+            _log.LogInformation("Loaded {RecipeCount} recipes", models.Count);
+            return View(models);
         }
     }
 }
