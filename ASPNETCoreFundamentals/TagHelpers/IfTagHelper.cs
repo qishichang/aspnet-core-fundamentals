b872fa83622f226ef0b10bf7efc4c308f9493fc2
qishichang <qishichang@live.com>
2019-06-03 22:27:19 +1200

    Create IfTagHelper to conditionally render elements

---
+++ ASPNETCoreFundamentals/TagHelpers/IfTagHelper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCoreFundamentals.TagHelpers
+{
+    [HtmlTargetElement(Attributes = "if")]
+    public class IfTagHelper : TagHelper
+    {
+        [HtmlAttributeName("if")]
+        public bool RenderContent { get; set; } = true;
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (RenderContent == false)
+            {
+                output.TagName = null;
+                output.SuppressOutput();
+            }
+            output.Attributes.RemoveAll("if");
+        }
+
+        public override int Order => int.MinValue;
+    }
+}
