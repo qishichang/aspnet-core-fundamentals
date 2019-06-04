082ac4eed8aed2f10e50bae86e607dcc8d1d806f
qishichang <qishichang@live.com>
2019-06-04 22:33:16 +1200

    Add custom validation attribute for currency codes

---
+++ ASPNETCoreFundamentals/DataAnnotations/CurrencyCodeAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCoreFundamentals.DataAnnotations
+{
+    public class CurrencyCodeAttribute : ValidationAttribute
+    {
+        private readonly string[] _allowedCodes;
+
+        public CurrencyCodeAttribute(params string[] allowedCodes)
+        {
+            _allowedCodes = allowedCodes;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var code = value as string;
+            if (code == null || !_allowedCodes.Contains(code))
+            {
+                return new ValidationResult("Not a valid currency code");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ ASPNETCoreFundamentals/Models/CurrencyConverterModel.cs
@@ -0,0 +1,26 @@
+﻿using ASPNETCoreFundamentals.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETCoreFundamentals.Models
+{
+    public class CurrencyConverterModel
+    {
+        [Required]
+        [StringLength(3, MinimumLength = 3)]
+        [CurrencyCode("GBP", "USD", "CAD", "EUR")]
+        public string CurrencyFrom { get; set; }
+
+        [Required]
+        [CurrencyCode("GBP", "USD", "CAD", "EUR")]
+        [StringLength(3, MinimumLength = 3)]
+        public string CurrencyTo { get; set; }
+
+        [Required]
+        [Range(1, 1000)]
+        public decimal Quantity { get; set; }
+    }
+}
