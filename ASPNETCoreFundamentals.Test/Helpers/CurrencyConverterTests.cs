4af9c3496c8cc62b91972d176efc29cec9999b4b
qishichang <qishichang@live.com>
2019-06-05 23:04:26 +1200

    Add unit test for ConverterToGbp using expected arguments

---
+++ ASPNETCoreFundamentals.Test/Helpers/CurrencyConverterTests.cs
@@ -0,0 +1,30 @@
+﻿using ASPNETCoreFundamentals.Helpers;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ASPNETCoreFundamentals.Test.Helpers
+{
+    public class CurrencyConverterTests
+    {
+        [Fact]
+        public void ConvertToGbp_ConvertsCorrectly()
+        {
+            // Arrange
+            var converter = new CurrencyConverter();
+            decimal value = 3;
+            decimal rate = 1.5m;
+            int dp = 4;
+
+            decimal expected = 2;
+
+            // Act
+            var actual = converter.ConvertToGbp(value, rate, dp);
+
+            // Assert
+            actual.ShouldBe(expected);
+        }
+    }
+}
