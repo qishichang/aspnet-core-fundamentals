bc26a9277147188b0de0e518024124851618c653
qishichang <qishichang@live.com>
2019-06-06 16:17:19 +1200

    Unit test HealthCheckMiddleware when a nonmatching path is provided

---
+++ ASPNETCoreFundamentals.Test/Middlewares/HealthCheckMiddlewareTests.cs
@@ -0,0 +1,37 @@
+﻿using ASPNETCoreFundamentals.Middlewares;
+using Microsoft.AspNetCore.Http;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ASPNETCoreFundamentals.Test.Middlewares
+{
+    public class HealthCheckMiddlewareTests
+    {
+        [Fact]
+        public async Task ForNonMatchingRequest_CallsNextDelegate()
+        {
+            // Arrange
+            var context = new DefaultHttpContext();
+            context.Request.Path = "/somethingelse";
+
+            var wasExecuted = false;
+            RequestDelegate next = (ctx) =>
+            {
+                wasExecuted = true;
+                return Task.CompletedTask;
+            };
+
+            var middleware = new HealthCheckMiddleware(next: next);
+
+            // Act
+            await middleware.Invoke(context);
+
+            // Assert
+            wasExecuted.ShouldBe(true);
+        }
+    }
+}
