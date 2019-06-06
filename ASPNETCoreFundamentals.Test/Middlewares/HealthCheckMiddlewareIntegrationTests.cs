4c777e44a36e52117cf24e3d170b3f0c52b24df9
qishichang <qishichang@live.com>
2019-06-06 20:01:24 +1200

    Create HealthCheckMiddleware integration test with TestServer

---
+++ ASPNETCoreFundamentals.Test/Middlewares/HealthCheckMiddlewareIntegrationTests.cs
@@ -0,0 +1,41 @@
+﻿using ASPNETCoreFundamentals.Middlewares;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.TestHost;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ASPNETCoreFundamentals.Test.Middlewares
+{
+    public class HealthCheckMiddlewareIntegrationTests
+    {
+        [Fact]
+        public async Task StatusMiddlewareReturnsPong()
+        {
+            var hostBuilder = new WebHostBuilder()
+                .Configure(app =>
+                {
+                    app.UseMiddleware<HealthCheckMiddleware>();
+                });
+
+            using(var server = new TestServer(hostBuilder))
+            {
+                HttpClient client = server.CreateClient();
+                var response = await client.GetAsync("/ping");
+
+                response.EnsureSuccessStatusCode();
+                var content = await response.Content.ReadAsStringAsync();
+
+                // Assert
+                content.ShouldBe("pong");
+            }
+        }
+    }
+}
