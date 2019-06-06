76e35658ec87868311ed98315be4054cb7af676b
qishichang <qishichang@live.com>
2019-06-06 21:56:38 +1200

    Add HomeController integration test

---
+++ ASPNETCoreFundamentals.Test/Controllers/HomeControllerIntegrationTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ASPNETCoreFundamentals.Test.Controllers
+{
+    public class HomeControllerIntegrationTests
+    {
+        [Fact]
+        public async Task IndexReturnsHtml()
+        {
+            var projectRootPath = Path.Combine(
+                Directory.GetCurrentDirectory(),
+                "..", "..", "..", "..", "ASPNETCoreFundamentals");
+
+            var builder = WebHost.CreateDefaultBuilder()
+                .UseStartup<Startup>()
+                .UseContentRoot(projectRootPath);
+
+            using (var server = new TestServer(builder))
+            {
+                HttpClient client = server.CreateClient();
+
+                var response = await client.GetAsync("/");
+
+                response.EnsureSuccessStatusCode();
+                var content = await response.Content.ReadAsStringAsync();
+                content.ShouldContain("<h1>Home page</h1>");
+            }
+        }
+    }
+}
