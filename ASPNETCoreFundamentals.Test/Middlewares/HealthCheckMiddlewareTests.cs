f5423ead547adda740ae9bcf77fc66e1967797a3
qishichang <qishichang@live.com>
2019-06-06 16:27:24 +1200

    Unit test HealthCheckMiddleware when a matching Path is provided

--- ASPNETCoreFundamentals.Test/Middlewares/HealthCheckMiddlewareTests.cs
+++ ASPNETCoreFundamentals.Test/Middlewares/HealthCheckMiddlewareTests.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Http;
 using Shouldly;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
@@ -32,6 +33,34 @@ namespace ASPNETCoreFundamentals.Test.Middlewares
 
             // Assert
             wasExecuted.ShouldBe(true);
+        }
+
+        [Fact]
+        public async Task ReturnsPongBodyContent()
+        {
+            // Arrange
+            var bodyStream = new MemoryStream();
+            var context = new DefaultHttpContext();
+            context.Response.Body = bodyStream;
+            context.Request.Path = "/ping";
+
+            RequestDelegate next = (ctx) => Task.CompletedTask;
+            var middleware = new HealthCheckMiddleware(next: next);
+
+            // Act
+            await middleware.Invoke(context);
+
+            string response;
+            bodyStream.Seek(0, SeekOrigin.Begin);
+            using (var reader = new StreamReader(bodyStream))
+            {
+                response = await reader.ReadToEndAsync();
+            }
+
+            // Assert
+            response.ShouldBe("pong");
+            context.Response.ContentType.ShouldBe("text/plain");
+            context.Response.StatusCode.ShouldBe(200);
         }
     }
 }
