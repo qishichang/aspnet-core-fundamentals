ac2ac0d7df69e74b193370f0bedb847906c333ff
qishichang <qishichang@live.com>
2019-06-11 22:51:59 +1200

    Add a custom claim to a new user when registering

---
+++ ASPNETCoreFundamentals/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ASPNETCoreFundamentals.Data;
+using ASPNETCoreFundamentals.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ASPNETCoreFundamentals.Areas.Identity.IdentityHostingStartup))]
+namespace ASPNETCoreFundamentals.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
