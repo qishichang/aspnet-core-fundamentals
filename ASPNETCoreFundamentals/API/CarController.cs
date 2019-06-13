2dbe1e4fd62e17f4e8a3a98312060b74a38b54e5
qishichang <qishichang@live.com>
2019-06-13 19:35:32 +1200

    Apply EnableCors attribute to a controller and action

--- ASPNETCoreFundamentals/API/CarController.cs
+++ ASPNETCoreFundamentals/API/CarController.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ASPNETCoreFundamentals.Models;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ASPNETCoreFundamentals.API
 {
+    [EnableCors("AllowShoppingApp")]
     [Route("api/[controller]", Name = "[controller]_[action]")]
     public class CarController : Controller
     {
+        [EnableCors("AllowAnyOrigin")]
         [Route("[action]")]
         [Route("ignition")]
         [Route("/start-car")]
@@ -26,6 +29,7 @@ namespace ASPNETCoreFundamentals.API
             return Ok($"The speed of the car is {speed}.");
         }
 
+        [DisableCors]
         [HttpPost]
         public IActionResult Add([FromBody]Car car)
         {
